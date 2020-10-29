340327676f3d589375becd0274772de87c1d465a
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-10-29 20:38:22 +0700

    recyclerview

--- app/src/main/java/com/example/fundamentalmyrecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/fundamentalmyrecyclerview/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.fundamentalmyrecyclerview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val list = ArrayList<Hero>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rv_heroes.setHasFixedSize(true)
+
+        list.addAll(getListHeroes())
+        showRecyclerList()
+    }
+
+    private fun showRecyclerList() {
+        rv_heroes.layoutManager = LinearLayoutManager(this)
+        rv_heroes.adapter = ListHeroAdapter(list)
+    }
+
+    private fun getListHeroes(): ArrayList<Hero> {
+        val dataName = resources.getStringArray(R.array.data_name)
+        val dataDescription = resources.getStringArray(R.array.data_description)
+        val dataPhoto = resources.getStringArray(R.array.data_photo)
+
+        val listHero = ArrayList<Hero>()
+        for (position in dataName.indices) {
+            listHero.add(
+                Hero(
+                    dataName[position],
+                    dataDescription[position],
+                    dataPhoto[position]
+                )
+            )
+        }
+        return listHero
     }
 }
